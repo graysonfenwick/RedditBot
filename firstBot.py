ea9df07b19b0a996293d6febe9f15841b14bdeaf
Grayson Fenwick <graysonfenwick@gmail.com>
2017-09-06 12:40:25 -0400

    Continued progress

--- firstBot.py
+++ firstBot.py
@@ -18,5 +18,27 @@ path = '~/Documents/Fun/RedditBot/temp.txt'
 
 def authenicate():
     print("Authenticating...")
-    reddit = praw.Reddit('nuzzyfipples', user_agent
+    reddit = praw.Reddit('nuzzyfipplesbot', user_agent='nuzzyfipplestestbot')
+    print("Authenticated as {}\n".format(reddit.user.me()))
+    return reddit
 
+
+def fetchdata(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    tag = soup.find('p')
+    data = ' '
+    while True:
+        if isinstance(tag bs4.element.Tag):
+            if (tag.name == 'h2'):
+                break
+            if (tag.name == 'h3'):
+                tag = tag.nextSibling
+            else:
+                data = data + '\n' + tag.text
+                tag = tag.nextSibling
+        else:
+            tag = tag.nextSibling
+
+    return data
